891fc43d5452f94b492990882d7a5510e0c51d82
Daniel Mendes <senseidanielmendes@gmail.com>
2023-09-11 21:34:37 -0300

    primeiro_registro_tabela_banco

---
+++ Loja_virtual/src/main/java/lojavirtual/danielmendes/model/ProductBrand.java
@@ -0,0 +1,41 @@
+package lojavirtual.danielmendes.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Table(name = "product_brand")
+@SequenceGenerator(name = "seq_product_brand", sequenceName = "seq_product_brand", allocationSize = 1, initialValue = 1)
+public class ProductBrand implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product_brand")
+	private Long id;
+
+	private String Name_Description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName_Description() {
+		return Name_Description;
+	}
+
+	public void setName_Description(String name_Description) {
+		this.Name_Description = name_Description;
+	}
+
+}
